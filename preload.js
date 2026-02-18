a721c4696b416eea16b180b559ae0ba0e049b5ca
Mohamed <amrmoh023@gmail.com>
2026-02-18 22:26:18 +0100

    chore: Delete Electron application source code and documentation.

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('db', {
-    loadData: () => ipcRenderer.invoke('db-load'),
-    saveData: (data) => ipcRenderer.invoke('db-save', data)
-});
